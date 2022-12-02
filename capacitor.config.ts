a17c4b401129bce9cdd7468f108c397c60e213ab
renz78 <yevtushenko.a78@gmail.com>
2022-12-02 10:04:40 +0200

    test

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nezlamnist',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
